11935a44b1a1689f081967d5286df851dbfb0b95
Toby Moszer <mosze006@umn.edu>
2021-02-22 22:50:26 -0600

    Fixing frame border offsets

--- src/ty48/Main.java
+++ src/ty48/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	private static final int sizeX = 10;
 	private static final int sizeY = 10;
-	static final int scale = 200;
+	static final int scale = 100;
 	
 	public static void main(String[] args) {
 		Board board = new Board(sizeX, sizeY);
@@ -14,7 +14,7 @@ public class Main {
 		JFrame frame = new JFrame("2048");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		frame.setSize(scale*sizeX, scale*sizeY + 57);
+		frame.setSize(scale*sizeX + 17, scale*sizeY + 40);
 		
 		MyPanel panel = new MyPanel(board);
 		
